285006c6b4f649266c85a4e886fdbb69f9760f4e
samuelmcmanus819 <samuelmcmanus819@gmail.com>
2020-07-31 13:51:10 -0500

    Add project files.

---
+++ TrollBot/TrollBot/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TrollBot.Models;
+
+namespace TrollBot.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
